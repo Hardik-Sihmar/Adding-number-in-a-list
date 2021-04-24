1a9f56663500b9c1df1395fb45c9f307488a78ac
Hardik-Sihmar <83064171+Hardik-Sihmar@users.noreply.github.com>
2021-04-24 14:19:16 +0530

    Add files via upload

---
+++ addingnumberinlist.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
+
+
+adding = [(num1 + 4), (num2 +4), (num3 + 4), (num4 + 4)]
+adding.sort()
+print(adding)
